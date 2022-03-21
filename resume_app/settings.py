5202f172674f1d0905be2c4f4e3134df87102eba
Bezunesh <zueb21@gmail.com>
2022-03-21 18:00:20 -0400

    fix: Limits allowed hosts, change secret-key to env variable

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v8bzoj5)*&_%x-yy7o*z-2$*m1uuo*hbtb(n)%@bboej@%wkox'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['www.bezunesh.com', 'bezunesh.com']
 
 
 # Application definition
